b3f177e162699e5b5259ad84590ace186f947b99
Ana Beatriz A. de M. da Silva <anabeatriz.augusto06@yahoo.com>
2021-01-03 20:25:29 +0000

    enviando 3

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,37 +1,53 @@
 def arithmetic_arranger(problems, mostrar=False):
 	lista = [x.split() for x in problems]
 	primeiro = [p[0] for p in lista]
+	operador = [o[1] for o in lista]
 	segundo = [s[2] for s in lista]
-	resposta = [eval(r) for r in problems]
-
-	if mostrar:
-		for i, v in enumerate(lista):
-			print(
-				v[0].rjust(2*3), "\n",
-				v[1], v[2].rjust(3), "\n", "-----".rjust(5), "\n",
-        str(resposta[i]).rjust(5), "\n"
-      )
-	else:
-		for i, v in enumerate(lista):
-			print(
-				v[0].rjust(2*3), "\n",
-				v[1], v[2].rjust(3), "\n", "-----".rjust(5), "\n"
-      )
-			#print("    ", end=" ")
 	
+	if mostrar:
+		resposta = [eval(r) for r in problems]
+		# Primeiro numero
+		for p in primeiro:
+			print(p.rjust(5), "    ", end=" ")
+		print()
+		# Operador e segundo numero
+		for os in range(len(operador)):
+			print(operador[os], segundo[os].rjust(3), "    ", end=" ")
+		print()
+		# Hifens
+		for s in range(len(segundo)):
+			print("-" * 5, "    ", end=" ")
+		print()
+		# Resposta
+		for r in resposta:
+			print(str(r).rjust(5), "    ", end=" ")
+		print()
+	else:
+		# Primeiro numero
+		for p in primeiro:
+			print(p.rjust(5), "    ", end=" ")
+		print()
+		# Operador e segundo numero
+		for os in range(len(operador)):
+			print(operador[os], segundo[os].rjust(3), "    ", end=" ")
+		print()
+		# Hifens
+		for s in range(len(segundo)):
+			print("-" * 5, "    ", end=" ")
+		print()
+
 	# Erros
 	if len(problems) > 5:
-		raise Exception("Error: Too many problems.")
+		return("Error: Too many problems.")
 
 	for c in primeiro + segundo:
 		if not c.isnumeric():
-			raise Exception("Error: Numbers must only contain digits.")
+			return("Error: Numbers must only contain digits.")
 		elif len(c) > 4:
-			raise Exception("Error: Numbers cannot be more than four digits.")
+			return("Error: Numbers cannot be more than four digits.")
 
 	for c in problems:
 		if not("+" not in c or "-" not in c):
-			raise Exception("Error: Operator must be '+' or '-'.")
+			return("Error: Operator must be '+' or '-'.")
 	
-
-arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49", "32 - 698"])
+arithmetic_arranger(["3 + 855", "3801 - 2", "45 + 43", "123 + 49"])
